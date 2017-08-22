72c57bdce0ae18bf7bdeeae3eb1ba05cde94c937
Gabor Heja <gheja@m00box.herd>
2017-08-23 01:17:05 +0200

    Updated landscape

--- landscape.js
+++ landscape.js
@@ -6,38 +6,65 @@ let canvas = null;
 let ctx = null;
 let body = null;
 let gui = null;
+let prng = null;
+let _seed = 0;
 
-const canvas_width = 416;
-const canvas_height = 234;
+const WIDTH = 1920;
+const HEIGHT = 1080;
+// const WIDTH = 480;
+// const HEIGHT = 270;
+const SCALE = HEIGHT / 400;
 
 let palette = [];
 
 let settings = {
-	h1: 0.77,
+	h1: 0,
 	s1: 0.2,
 	l1: 0.3,
 	
-	h2: 0.15,
+	h2: 0.16,
 	s2: 1.0,
-	l2: 0.5,
+	l2: 0.59,
 	
-	height: 1.5,
+	pow: 3.42,
 	
-	mode: 1,
-	pow: 0.5,
+	density: 0.32,
 	
-	autoUpdate: false
+	autoUpdate: false,
+	
+	moons: 0
 };
 
-// function lerp(a, b, x)
-// {
-// 	return a + (b - a) * (x * x);
-// }
+
+//// lib
+
+function _scale(x)
+{
+	return x * SCALE;
+}
+
+function clamp(x, min, max)
+{
+	if (x < min)
+	{
+		return min;
+	}
+	
+	if (x > max)
+	{
+		return max;
+	}
+	
+	return x;
+}
 
 function lerp(a, b, x)
 {
 	return a + (b - a) * Math.pow(x, settings.pow);
 }
+
+
+//// landscape
 
 function convert(p, q, t)
 {
@@ -83,22 +110,39 @@ function hsla2rgba_(h, s, l, a)
 
 function buildPalette()
 {
-	let i;
+	let i, a;
 	
 	for (i=0; i<PALETTE_LENGTH; i++)
 	{
+		a = i / PALETTE_LENGTH;
+		
 		palette[i] = hsla2rgba_(
-			lerp(settings.h1, settings.h2, i/PALETTE_LENGTH),
-			lerp(settings.s1, settings.s2, i/PALETTE_LENGTH),
-			lerp(settings.l1, settings.l2, i/PALETTE_LENGTH),
-			1
+			lerp(settings.h1, settings.h2, a),
+			lerp(settings.s1, settings.s2, a),
+			lerp(settings.l1, settings.l2, a),
+			Math.pow(a, 1 - settings.density * 0.88)
 		);
+	}
+}
+
+function ball(ctx, x, y, r)
+{
+	let i, b;
+	const a = 500;
+	
+	for (i=0; i<a; i++)
+	{
+		b = i/a;
+		
+		ctx.beginPath();
+		ctx.arc(x + (b/2) * r, y + (b/2) * r, r * (b * b * b), 0, 2 * Math.PI);
+		ctx.fill();
 	}
 }
 
 function draw()
 {
-	let i, n, c1, c2, a;
+	let i, n, c1, c2, a, p1, p2;
 	
 	a = 1;
 	
@@ -107,32 +151,81 @@ function draw()
 		buildPalette();
 	}
 	
+	ctx.globalCompositeOperation = 'source-over';
 	ctx.fillStyle = "#000";
-	ctx.fillRect(0, 0, 300, 300);
+	ctx.fillRect(0, 0, WIDTH, HEIGHT);
+	ctx.fillStyle = "#fff";
 	
-	if (settings.mode == 1)
+	prng.seed = _seed;
+	
+	ctx.fillStyle = "rgba(255,255,255,0.5)";
+	for (i=0; i<50; i++)
 	{
-		for (i=0; i<canvas_height; i++)
-		{
-			a = i * settings.height;
-			
-			n = Math.floor(i / canvas_height * PALETTE_LENGTH);
-			
-			ctx.fillStyle = palette[n];
-			
-			ctx.fillRect(0, i, canvas_width, 1);
-		}
+		ctx.beginPath();
+		ctx.arc(prng.random() * WIDTH, prng.random() * HEIGHT, _scale(1), 0, 2 * Math.PI);
+		ctx.fill();
 	}
+	
+	// sun color!
+	ctx.fillStyle = "rgba(255,230,190,0.1)";
+	
+//	for (i=0; i<settings.moons; i++)
+	{
+		ball(ctx, prng.random() * WIDTH, prng.random() * HEIGHT / 2, _scale(5 + 25 * prng.random()));
+	}
+//	ctx.globalCompositeOperation = 'screen';
+	
+	for (i=0; i<HEIGHT; i++)
+	{
+		n = Math.floor(i / HEIGHT * PALETTE_LENGTH);
+		
+		ctx.fillStyle = palette[n];
+		ctx.fillRect(0, i, WIDTH, 1);
+	}
+	
+	ctx.globalCompositeOperation = 'screen';
+	
+	// sun color!
+	a = _scale(30 + 30 * prng.random());
+	p1 = prng.random() * WIDTH;
+	p2 = prng.random() * HEIGHT / 2 + HEIGHT / 2;
+	
+	ctx.fillStyle = "rgba(255,255,255,0.3)";
+	ball(ctx, p1, p2, a);
 	
 	_raf(draw);
 }
 
+function randomize()
+{
+	_seed = Math.floor(Math.random() * 1000000);
+	
+	settings.h1 = Math.random();
+	if (Math.random() < 0.5)
+	{
+		settings.h2 = settings.h1 - 0.1 - Math.random() * 0.5;
+	}
+	else
+	{
+		settings.h2 = settings.h1 + 0.1 + Math.random() * 0.5;
+		settings.h2 = settings.h1 + 0.1 + Math.random() * 0.5;
+	}
+	settings.s2 = 0.2 + Math.random() * 0.5;
+	settings.pow = Math.random();
+	settings.density = Math.random();
+	settings.moons = Math.floor(Math.pow(Math.random(), 4) * 3);
+}
+
 function init()
 {
+	let tmp;
+	
+	prng = new AlmostRandom();
+	
 	canvas = document.createElement("canvas");
 	
-	canvas.width = canvas_width;
-	canvas.height = canvas_height;
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
 	ctx = canvas.getContext("2d");
 	
 	body = document.body;
@@ -140,21 +233,26 @@ function init()
 	
 	settings.update = buildPalette;
 	
+	randomize();
+	
 	gui = new dat.gui.GUI();
 	
-	gui.add(settings, 'h1').min(0).max(1).step(0.01);
-	gui.add(settings, 's1').min(0).max(1).step(0.01);
-	gui.add(settings, 'l1').min(0).max(1).step(0.01);
+	tmp = gui.addFolder("Atmosphere");
 	
-	gui.add(settings, 'h2').min(0).max(1).step(0.01);
-	gui.add(settings, 's2').min(0).max(1).step(0.01);
-	gui.add(settings, 'l2').min(0).max(1).step(0.01);
+	tmp.add(settings, 'h1').min(0).max(1).step(0.01);
+	tmp.add(settings, 's1').min(0).max(1).step(0.01);
+	tmp.add(settings, 'l1').min(0).max(1).step(0.01);
 	
-	gui.add(settings, 'mode').min(1).max(2).step(1);
-	gui.add(settings, 'pow').min(0.1).max(10);
+	tmp.add(settings, 'h2').min(0).max(1).step(0.01);
+	tmp.add(settings, 's2').min(0).max(1).step(0.01);
+	tmp.add(settings, 'l2').min(0).max(1).step(0.01);
 	
-	gui.add(settings, 'height').min(0).max(10).step(0.01);
-	gui.add(settings, 'autoUpdate');
+	tmp.add(settings, 'pow').min(0.1).max(10);
+	
+	tmp.add(settings, 'density').min(0).max(1).step(0.01);
+	tmp.add(settings, 'autoUpdate');
+	
+	tmp.open();
 	
 	buildPalette();
 	
